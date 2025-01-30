b428c46cae65cdadaac1a02224af485aa7197fba
basspies <haroldspies@hotmail.com>
2025-01-30 01:00:34 +0100

    web development
    alle 3 de opdrachten van webdevelopment

--- index.php
+++ index.php
@@ -10,11 +10,15 @@ if(!empty($_POST)){
 
     //dit is een voorbeeld array.  Deze waardes moeten erin staan.
     $postArray = [
-        'name' => "Ieniminie",
-        'email' => "ieniminie@sesamstraat.nl",
-        'message' => "Geweldig dit"
+        'name' => $_POST["name"],
+        'email' => $_POST["comment"],
+        'message' => $_POST["comment"]
+        
+        // 'name' => "Ieniminie",
+        // 'email' => "ieniminie@sesamstraat.nl",
+        // 'message' => "Geweldig dit"
     ];
-
+    
     $setReaction = Reactions::setReaction($postArray);
 
     if(isset($setReaction['error']) && $setReaction['error'] != ''){
@@ -33,10 +37,34 @@ if(!empty($_POST)){
     <title>Youtube remake</title>
 </head>
 <body>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=twI61ZGDECBr4ums" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/zrUWSfd0Heg?si=YTlifPv8ytz_Vr_fs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
     <h2>Hieronder komen reacties</h2>
-    <p>Maak hier je eigen pagina van aan de hand van de opdracht</p>
+    <form action="" method="POST">
+        <label for="fname">name</label><br>
+        <input type="text" id=fname name="name" value=""><br> 
+        <label for="lname">email</label><br>
+        <input type="text" id=lname name="email" value=""><br><br>
+        <label for="lname">comment</label><br>
+        <textarea id="w3review" name="comment" rows="4" cols="50"></textarea>
+        <input type="submit" value="verzenden"> 
+    </form>
+   
+    <p>Maak hier je eigen pagina van aan de hand van de opdracht</p> 
+<?php
+foreach ($getReactions as $reaction){
+    echo("<div class='message'>");
+    echo "<h3>".$reaction['name']."</h3>";
+    echo "<p>".$reaction['message']."</p>";
+    echo ("</div>");
+}
+
+
+
+?>
+
+
+
 </body>
 </html>
 
